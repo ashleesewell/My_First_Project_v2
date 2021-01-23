839a05ac8e15528ddbcc899bd05d980537b62881
Ashlee Sewell <ashleesewell@usf.edu>
2021-01-22 21:14:51 -0500

    Add project files.

---
+++ My_First_Project_v2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace My_First_Project_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pause to display hello World! 
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
